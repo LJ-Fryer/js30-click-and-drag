2ec8a95340ff7b378adcdced6990a8ee45fe96db
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-12 14:26:01 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Testing");
+const slider = document.querySelector(".items");
+
+let isDown = false;
+let startX; // intial click value - mousedown
+let scrollX;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.x - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; // if isDown is false, stop function & stop returning events
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5;
+  slider.scrollLeft = scrollLeft - walk;
+});
